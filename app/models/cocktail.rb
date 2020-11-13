99291ab23107781cce56cf0d5d6e012c76c7b3d4
marine payet <marine.payet3@gmail.com>
2020-11-13 17:56:35 +0100

    ok

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,8 +3,9 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
 
   validates :name, presence: true, uniqueness: true
+
+  has_one_attached :photo
 end
 
 
-# Doit-on creer une view new de chaque model ? pour le form ? ET LE  mettre
-#sur la view new du cocktail ?
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,20 @@ require 'faker'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Ingredient.destroy_all
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-
-puts "ok pour les 3"
+puts "starts"
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
 ingredient = JSON.parse(ingredients_serialized)
 
 
 ingredient['drinks'].each do |row|
-  Ingredient.create(name: row['strIngredient1'])
+  Ingredient.create!(name: row['strIngredient1'])
 end
 
-20.times do
-  Cocktail.create(name: Faker::Food.fruits)
+10.times do
+  Cocktail.create!(name: Faker::Food.fruits)
 end
 puts "finiii"
